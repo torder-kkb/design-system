c037f210359f4186b3c932e0176df67735d2d60a
torder-kkb <kkb5@torder.com>
2024-01-12 17:27:13 +0900

    init

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>나는 디자인 시스템 버튼</button>;
+};
+
+export default Button;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+const Input = () => {
+  return <input placeholder="나는 디자인 시스템 인풋" />;
+};
+
+export default Input;
